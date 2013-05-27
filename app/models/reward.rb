cc088db7dbbeeaed93b67c7f3d46208f4df6b8b8
Ledniov Vladislav <vlad.ledniov@gmail.com>
2013-05-27 15:07:50 +0300

    Added Reward model

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -1,18 +1,19 @@
-class Goal < ActiveRecord::Base
+class Reward < ActiveRecord::Base
   before_create :create_timestamps
   before_update :update_timestamps
 
-  has_many :rewards
+  belongs_to :goal
 
-  validates_presence_of :date
-  validates_presence_of :name
+  validates_presence_of :creator_id
+  validates_presence_of :datetime
   validates_presence_of :user_id
 
-  attr_accessible :name, :date
+  attr_accessible :datetime
 
   def create_timestamps
     self.created = Time.now
     self.modified = Time.now
+    self.is_hidden = false
   end
 
   def update_timestamps
