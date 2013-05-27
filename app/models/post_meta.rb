da2be8cd2d96d713f6d95639674e8e0c96eab41d
Ledniov Vladislav <vlad.ledniov@gmail.com>
2013-05-27 12:25:39 +0300

    Deleted unnecessary models, generated from wp database

--- app/models/post_meta.rb
+++
@@ -1,9 +0,0 @@
-class PostMeta < ActiveRecord::Base
-  self.table_name = "wp_postmeta"
-  belongs_to :post
-
-  alias_attribute :id, :meta_id
-  alias_attribute :key, :meta_key
-  alias_attribute :value, :meta_value
-  
-end
--- app/models/user_meta.rb
+++
@@ -1,8 +0,0 @@
-class UserMeta < ActiveRecord::Base
-  self.table_name = "wp_usermeta"
-  belongs_to :user
-
-  alias_attribute :id, :umeta_id
-  alias_attribute :key, :meta_key
-  alias_attribute :value, :meta_value
-end
