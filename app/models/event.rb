76621fbcbf034881cb88555b6f7b55ce2e80b7e5
Ledniov Vladislav <vlad.ledniov@gmail.com>
2013-05-27 14:09:37 +0300

    Added event model

---
+++ app/models/event.rb
@@ -0,0 +1,24 @@
+class Event < ActiveRecord::Base
+  before_create :create_timestamps
+  before_update :update_timestamps
+
+  validates_presence_of :brand_page_id
+  validates_presence_of :end_time
+  validates_presence_of :start_time
+  validates_presence_of :message_id
+  validates_presence_of :name
+  validates_presence_of :user_id
+  validates_presence_of :venue
+
+  attr_accessible :end_time, :start_time, :name, :venue
+
+  def create_timestamps
+    self.created = Time.now
+    self.modified = Time.now
+    self.is_public = true
+  end
+
+  def update_timestamps
+    self.modified = Time.now
+  end
+end
