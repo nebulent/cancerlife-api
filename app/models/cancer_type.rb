b1872c132a6471d7b0445dbfcec0515a714f0562
Ledniov Vladislav <vlad.ledniov@gmail.com>
2013-05-27 13:12:20 +0300

    Added cancer_type model;

---
+++ app/models/cancer_type.rb
@@ -0,0 +1,17 @@
+class CancerType < ActiveRecord::Base
+  before_create :create_timestamps
+  before_update :update_timestamps
+
+  validates_presence_of :name
+  validates_presence_of :user_count
+
+  attr_accessible :name, :user_count
+  def create_timestamps
+    self.created = Time.now
+    self.modified = Time.now
+  end
+
+  def update_timestamps
+    self.modified = Time.now
+  end
+end
