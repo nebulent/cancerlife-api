58ccd68b66c14bb81520523195164353d7963899
Vasilica Victor <vasilica.victor@gmail.com>
2013-05-28 15:11:04 +0300

    Added user_treatment_dosage model

---
+++ app/models/user_treatment_dosage.rb
@@ -0,0 +1,20 @@
+class UserTreatmentDosage < ActiveRecord::Base
+  before_create :update_timestamps
+  before_update :update_modified_time
+
+  validates_presence_of :days
+  validates_presence_of :dosage
+  validates_presence_of :time
+  validates_presence_of :user_treatment_id
+  
+  attr_accessible :days, :dosage, :time 
+
+  def update_timestamps
+    self.created = Time.now
+    self.modified = Time.now
+  end
+
+  def update_modified_time
+    self.modified = Time.now
+  end
+end
