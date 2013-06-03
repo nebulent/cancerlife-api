d7d539b3c408bd18c2bf4807329eab01e58be2b5
Ledniov Vladislav <vlad.ledniov@gmail.com>
2013-06-03 12:26:00 +0300

    Changed routing for API requests

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,14 @@
 Cancerlife::Application.routes.draw do
 
   resources :users
+
+  namespace :api do
+    resources :users, :only => :index do
+      collection do
+        post 'create', :format => :json, :path => 'create'
+     end
+    end
+  end
   
-  root :to => "pages#index", :via => :post, :format => :json
+  root :to => "pages#index"
 end
