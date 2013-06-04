0ceb55e45605c8023b752a956881b121daa3132d
Ledniov Vladislav <vlad.ledniov@gmail.com>
2013-06-04 16:01:37 +0300

    Added authorization, using access tokens

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,5 +1,5 @@
 class Api::UsersController < ApplicationController
-
+  before_filter :restrict_access
   respond_to :json
 
   def create
@@ -8,8 +8,15 @@ class Api::UsersController < ApplicationController
       @response = {:message => "The user has been saved"}
       respond_with(@response)
     else
-      @response = {:message => "There was an error saving the user", :errors => @user.errors.messages}
+      @response = {:message => "There was an error saving the user",
+                   :errors => @user.errors.messages}
       respond_with(@response)
+    end
+  end
+
+  def restrict_access
+    authenticate_or_request_with_http_token do |token, options|
+      ApiToken.exists?(:access_key => token)
     end
   end
 end
