6b84409ff4297c7af7eaa050649a5f86fb538ca1
Ledniov Vladislav <vlad.ledniov@gmail.com>
2013-06-05 13:07:58 +0300

    Add subdomains for API

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,14 @@ Cancerlife::Application.routes.draw do
 
   resources :users
 
-  namespace :api do
-    resources :users, :only => :index do
-      collection do
-        post 'create', :format => :json, :path => 'create'
-     end
+  constraints(Subdomain) do
+    namespace :api do
+      resources :users, :only => :index do
+        collection do
+          post 'create', :format => :json, :path => 'create'
+       end
+      end
     end
   end
-  
   root :to => "pages#index"
 end
