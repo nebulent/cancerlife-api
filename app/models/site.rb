9c3ceae0abc245f69c2f79c6cedac06d45d79c71
Ledniov Vladislav <vlad.ledniov@gmail.com>
2013-06-05 15:25:28 +0300

    Added validation for subdomains

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,5 +1,9 @@
 class Site < ActiveRecord::Base
   has_and_belongs_to_many :users
 
+  validates_presence_of :subdomain
+  validates_uniqueness_of :subdomain
+  validates_format_of :subdomain, :with => /^[a-z\d]+(-[a-z\d]+)*$/i
+
   attr_accessible :subdomain
 end
