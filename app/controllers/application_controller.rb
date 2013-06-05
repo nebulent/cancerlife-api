4623b4cc192d0427fb66869d3fd744ba9f251fbc
Ledniov Vladislav <vlad.ledniov@gmail.com>
2013-06-05 14:01:51 +0300

    Added current_tenant_id helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  helper_method :current_tenant_id
+
+  def current_tenant_id
+    Site.find_by_subdomain(request.subdomain).id
+  end
 end
