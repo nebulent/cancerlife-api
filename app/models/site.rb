862b49373ae7d2ef1f636f07796335c44b8863f2
Ledniov Vladislav <vlad.ledniov@gmail.com>
2013-06-06 15:44:52 +0300

    Added error validation message when creating subdomains

--- app/models/site.rb
+++ app/models/site.rb
@@ -3,7 +3,7 @@ class Site < ActiveRecord::Base
 
   validates_presence_of :subdomain
   validates_uniqueness_of :subdomain
-  validates_format_of :subdomain, :with => /^[a-z\d]+(-[a-z\d]+)*$/i
+  validates_format_of :subdomain, :with => /^[a-z\d]+(-[a-z\d]+)*$/i, :message => "has to be only characters, dashes and numbers"
 
   attr_accessible :subdomain
 end
